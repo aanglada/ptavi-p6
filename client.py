e40e9f2808c7323d7f924485bafc5a497178ec74
Antonio Nunez Anglada <aanglada@alpha07.aulas.gsyc.urjc.es>
2018-11-19 15:50:59 +0100

    Me da un error raro: ERROR: ld.so: object '/lib/x86_64-linux-gnu/libsnoopy.so' from /etc/ld.so.preload cannot be preloaded (wrong ELF class: ELFCLASS64): ignored.

--- client.py
+++ client.py
@@ -12,7 +12,6 @@ import sys
 # Dirección IP del servidor.
 
 try:
-	SERVER = 'localhost'
 	METHOD = sys.argv[1]
 	LOGIN = sys.argv[2].split('@')[0]
 	IP = sys.argv[2].split('@')[1].split(':')[0]
@@ -29,14 +28,14 @@ LINE = METHOD + ' sip:' + LOGIN + '@' + IP + ':' + str(PORT) + 'SIP/2.0\r\n'
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((IP, PORT))
 
     print("Enviando: " + LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
 
     line = data.decode('utf-8').split('\r\n')
-    if line[0].split()[-1] == 'Trying' and method == 'INVITE':
+    if line[0].split()[-1] == 'Trying' and METHOD == 'INVITE':
         print(line[0] + '\n' + line[1] + '\n' + line[2])
         my_socket.send(bytes(LINE.replace('INVITE', 'ACK'), 'utf-8') + b'\r\n')
     else:
--- server.py
+++ server.py
@@ -4,28 +4,55 @@
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
+import os
 import socketserver
+import sys
 
+try:
+        IP = sys.argv[1]
+        PORT = int(sys.argv[2])
+        FILE= sys.argv[3]
+except:
+    sys.exit("Usage: python3 server.py IP port audio_file")
+
+if not os.path.exists(FILE):
+        sys.exit("Document doesn't exist")
+
+TRYING = b"SIP/2.0 100 Trying\r\n"
+RINGING = b"SIP/2.0 180 Ringing\r\n"
+OK = b"SIP/2.0 200 OK\r\n"
+BAD_REQUEST = b"SIP/2.0 400 Bad Request\r\n"
+Not_Allowed = b"SIP/2.0 405 Method Not Allowed\r\n"
+aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " + FILE
 
 class EchoHandler(socketserver.DatagramRequestHandler):
-    """
-    Echo server class
-    """
-
+ 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
-
+       
+        message = self.rfile.read()
+        line = message.decode('utf-8').split(' ')
+        METHOD = line[0]
+        if len(line)<4 and len(line)>0:
+                if METHOD == 'INVITE':
+                   print('INVITE received')
+                   self.wfile.write(TRYING + RINGING + OK + b'\r\n')
+                elif METHOD == 'ACK':
+                   print("ACK RECIVED")
+                   os.system(aEjecutar)
+                elif METHOD == 'BYE':
+                   print("BYE RECIVED")
+                   self.wfile.write(OK + b'\r\n')
+                else:
+                   self.wfile.write(Not_Allowed)
+        else:
+             self.wfile.write(BAD_REQUEST) 
+                        
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    print('Listening...')
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
+        
