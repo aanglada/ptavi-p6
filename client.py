05437d7076e4116f03a449421d5418a59b2dbde3
Antonio Nunez Anglada <aanglada@alpha30.aulas.gsyc.urjc.es>
2018-11-21 13:26:06 +0100

    Corrección de errores

--- client.py
+++ client.py
@@ -23,7 +23,7 @@ except (IndexError, ValueError):
 
 # Contenido que vamos a enviar
 
-LINE = METHOD + ' sip:' + LOGIN + '@' + IP + ':' + str(PORT) + 'SIP/2.0\r\n'
+LINE = METHOD + ' sip:' + LOGIN + '@' + IP + ':' + str(PORT) + ' SIP/2.0\r\n'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
@@ -33,12 +33,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     print("Enviando: " + LINE)
     my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
-
-    line = data.decode('utf-8').split('\r\n')
-    if line[0].split()[-1] == 'Trying' and METHOD == 'INVITE':
-        print(line[0] + '\n' + line[1] + '\n' + line[2])
-        my_socket.send(bytes(LINE.replace('INVITE', 'ACK'), 'utf-8') + b'\r\n')
-    else:
-        print(line[0])
-
+    Recieve = data.decode('utf-8').split()
+    print(Recieve)
+    if Recieve[1] == "100":
+        ack ='ACK sip:' + LOGIN + '@' + IP + ' SIP/2.0\r\n'
+        my_socket.send(bytes(ack, 'utf-8') + b'\r\n')
+        print('Enviando: ' + ack)
+    print('Terminando socket...')
+    
 print("Fin.")
--- server.py
+++ server.py
@@ -9,14 +9,14 @@ import socketserver
 import sys
 
 try:
-        IP = sys.argv[1]
-        PORT = int(sys.argv[2])
-        FILE= sys.argv[3]
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    FILE= sys.argv[3]
 except:
     sys.exit("Usage: python3 server.py IP port audio_file")
 
 if not os.path.exists(FILE):
-        sys.exit("Document doesn't exist")
+    sys.exit("Document doesn't exist")
 
 TRYING = b"SIP/2.0 100 Trying\r\n"
 RINGING = b"SIP/2.0 180 Ringing\r\n"
@@ -26,33 +26,50 @@ Not_Allowed = b"SIP/2.0 405 Method Not Allowed\r\n"
 aEjecutar = "./mp32rtp -i 127.0.0.1 -p 23032 < " + FILE
 
 class EchoHandler(socketserver.DatagramRequestHandler):
- 
+
+    def Comprobar_Peticion(self):
+        if len(self.line) == 3:
+            sip_condition = self.line[1].split(":")[0] == ("sip")
+            final_condition = self.line[2] == ("SIP/2.0\r\n\r\n")
+            arroba_condition = False
+        if self.line[1].find("@") != -1:
+            arroba_condition = True
+        if sip_condition and arroba_condition and final_condition:
+            self.check = True
+        return self.check
+
     def handle(self):
-       
+
+        self.check = False
         message = self.rfile.read()
-        line = message.decode('utf-8').split(' ')
-        METHOD = line[0] 
-        if len(line)<4 and len(line)>0:
+
+        if message:
+            self.line = message.decode('utf-8').split(' ')
+            METHOD = self.line[0]
+
+            if self.Comprobar_Peticion():
                 if METHOD == 'INVITE':
-                   print('INVITE received')
-                   self.wfile.write(TRYING + RINGING + OK + b'\r\n')
+                    print('INVITE received')
+                    self.wfile.write(TRYING + RINGING + OK + b'\r\n')
                 elif METHOD == 'ACK':
-                   print("ACK RECIVED")
-                   os.system(aEjecutar)
+                    print("ACK RECIVED")
+                    os.system(aEjecutar)
                 elif METHOD == 'BYE':
-                   print("BYE RECIVED")
-                   self.wfile.write(OK + b'\r\n')
+                    print("BYE RECIVED")
+                    self.wfile.write(OK + b'\r\n')
                 else:
-                   self.wfile.write(Not_Allowed)
+                    self.wfile.write(Not_Allowed + b'\r\n')
+            else:
+                self.wfile.write(BAD_REQUEST + b'\r\n')
         else:
-             self.wfile.write(BAD_REQUEST) 
-                        
+            pass
+
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', PORT), EchoHandler)
+    server = socketserver.UDPServer((IP, PORT), EchoHandler)
     print('Listening...')
     try:
-        serv.serve_forever()
+        server.serve_forever()
     except KeyboardInterrupt:
         print("Finalizado servidor")
-        
+
